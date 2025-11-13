fefcdcc17f8b3460f41acd5348fb8579d4187cc2
Neslinur <snsoykan13@gmail.com>
2025-11-14 00:32:54 +0300

    added portfolio link under carousel for mobile screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Carousel } from "@/components/Carousel";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <Carousel />
+      <Link className="flex justify-end text-xl mr-10 mt-10 sm:hidden" href="/portfolio">See all artworks</Link>
     </main> 
   );
 }

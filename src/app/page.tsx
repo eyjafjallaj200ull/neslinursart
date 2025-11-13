01d955279ac3986716735cffe01466706383e159
Neslinur <snsoykan13@gmail.com>
2025-11-14 01:29:05 +0300

    adjusted the padding of the link under carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Carousel />
-      <Link className="float-right mr-5 mt-10 text-lg p-1 rounded-md border-solid border-foreground border-1 sm:hidden" href="/portfolio">See all artworks</Link>
+      <Link className="float-right mr-5 mt-10 text-lg px-2 pb-1 pt-0.25 rounded-md border-solid border-foreground border-1 sm:hidden" href="/portfolio">See all artworks</Link>
     </main> 
   );
 }

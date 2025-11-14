f230a9b2cc84638a029b43b7e9b5ceec983aab21
Neslinur <snsoykan13@gmail.com>
2025-11-14 11:54:25 +0300

    set priority for the new LCP on homepage

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -14,8 +14,8 @@ export function Carousel() {
     return (
         <div className='overflow-hidden' ref={emblaRef}>
             <div className='flex gap-3'>
-                <Image className='max-h-[400px] sm:max-h-[600px] ml-3 object-cover' src={Passage} alt="Passage"  sizes="(min-width: 2460px) calc(25.37vw - 159px), (min-width: 640px) 429px, 291px" />
-                <Image className='max-h-[400px] sm:max-h-[600px] object-cover' priority={true} src={Neighbourhood} alt="A Neighbourhood" sizes="(min-width: 2500px) calc(32.8vw - 118px), (min-width: 2180px) calc(82.67vw - 1360px), (min-width: 640px) 408px, 278px" />
+                <Image className='max-h-[400px] sm:max-h-[600px] ml-3 object-cover' priority={true} src={Passage} alt="Passage"  sizes="(min-width: 2460px) calc(25.37vw - 159px), (min-width: 640px) 429px, 291px" />
+                <Image className='max-h-[400px] sm:max-h-[600px] object-cover' src={Neighbourhood} alt="A Neighbourhood" sizes="(min-width: 2500px) calc(32.8vw - 118px), (min-width: 2180px) calc(82.67vw - 1360px), (min-width: 640px) 408px, 278px" />
                 <Image className='max-h-[400px] sm:max-h-[600px] object-cover' src={Forest} alt="The Forest of the Moss Spirit" sizes="(min-width: 2860px) 15.71vw, (min-width: 640px) 407px, 275px" />
                 <Image className='max-h-[400px] sm:max-h-[600px] object-cover' src={City} alt="City" sizes="(min-width: 2700px) 17vw, (min-width: 640px) 422px, 285px" />
                 <Image className='max-h-[400px] sm:max-h-[600px] object-cover' src={Underworld} alt="Entrance of the Underworld" sizes="(min-width: 2660px) 17.65vw, (min-width: 640px) 420px, 284px" />

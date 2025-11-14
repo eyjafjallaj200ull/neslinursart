37d17a86ee0e34aaa3a110a9c40c99d5b252d73b
Neslinur <snsoykan13@gmail.com>
2025-11-14 18:09:17 +0300

    order portfolio artworks by name

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import Gallery from "@/components/Portfolio/Gallery";
 import { getImageDimensionsByImageId } from "@/dal/imageDimensions";
 
 export default async function PortfolioPage() {
-  const artworks = await getArtworks({orderBy: false})
+  const artworks = await getArtworks({orderBy: true})
   const dimensionsArray = await Promise.all(artworks.map( async (artwork) => {
       const dimensions = await getImageDimensionsByImageId(artwork.id)
       return {id: artwork.id, dimensions}
